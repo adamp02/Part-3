8923b0d7ba03f4ddd0244be8f6f385740c355625
AdamP <62314569+adamp02@users.noreply.github.com>
2024-04-02 16:03:10 -0400

    Added more GUI information
    Added player's health and hunger level to the game system GUI

--- Assets/Assignment/Scripts/PlayerMove.cs
+++ Assets/Assignment/Scripts/PlayerMove.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -11,6 +12,12 @@ public class PlayerMove : MonoBehaviour
     public int health = 5;
     public int hunger = 5;
 
+    public int maxHealth = 10;
+    public int maxHunger = 10;
+
+    // gui stuff
+    [SerializeField] private TextMeshProUGUI healthText;
+    [SerializeField] private TextMeshProUGUI hungerText;
 
     Vector2 direction;
 
@@ -47,12 +54,26 @@ public class PlayerMove : MonoBehaviour
     {
         Debug.Log("You ate some food!");
         hunger += foodValue;
+
+        if (hunger > maxHunger)
+        {
+            hunger = maxHunger;
+        }
+
+        hungerText.SetText("Hunger: " + hunger + " / " + maxHunger);
     }
 
     public void HealthPickup(int healthValue)
     {
         Debug.Log("You got a medkit!");
         health += healthValue;
+
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+
+        healthText.SetText("Health: " + health + " / " + maxHealth);
     }
 
     public void SpeedPickup(int speedIncrease)
